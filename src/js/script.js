d8eecc362092a5e479b3854177629b911d7cc68e
L_Dem_L <ostapvasilenko55@gmail.com>
2023-01-14 14:56:15 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,39 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
+let name = 'Ostap'
+let surname = 'Vasylenko'
+let middleName = 'Andriyovych'
+
+let arraySome = [2, 13, 6, 13, 56];
+const position = arraySome.indexOf('6');
+let someStr = "sdsdsdfsg"
+try{
+
+}catch{
+
+}
+
+if(position >= 0){
+  console.log(`Число ${6} є в масиві`)
+}else{
+  console.log(`Число ${6} не є в масиві`)
+}
+
+// arraySome.splice(2, 1)
+
+// const position = arraySome.indexOf('hello');
+// arraySome.splice(position, 1)
+
+
+// console.log('Array:', arraySome)
+
+//Array
+
+// let age =[12, 13, 22, 13, 17];
+
+
+// const personalData = surname + '.' + ' ' + name[0] + '.' + ' ' + middleName[0]
+
+
+
+// console.log('Data :', personalData);
+
 
